97642d2b07dae00ea6dde263e0c6a8367459e331
sumukh2 <71953405+sumukh2@users.noreply.github.com>
2021-01-27 20:03:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function curse(){
+    window.alert("this is a highly cursed project");
+}
+function box(){
+    window.location="box_pen.html"
+}
+function com(){
+    window.location="virsu.html"
+}
+function room(){
+    window.location="nose.html"
+}
+function room2(){
+    window.location="apple.html"
+}
+function in_a_box(){
+    window.location="person.html"
+}
